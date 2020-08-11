1d0cd4db498fd7f209d6b1b31309bcfa89099702
Sergio Pastor Ontiveros Pérez <sergio.ontiveros.perez@outlook.com>
2020-08-11 16:18:07 -0300

    A new theme was added

---
+++ Lecture/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lecture.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lecture.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult showStudent()
+        {
+            var student = new Student()
+            {
+                Name = "Sergio",
+                Surname = "Ontiveros",
+                Email = "sergio@mail.com"
+            };
+
+            return View(student);
+        }
+    }
+}
