f7debc73f5abcea3a749c2bd3bc82b24efad5f0e
Sergio Pastor Ontiveros Pérez <sergio.ontiveros.perez@outlook.com>
2020-08-28 10:52:09 -0300

    Add StringLength to Email in ProfessorsModel

---
+++ Lecture/Migrations/20200828134956_AddStringLengthToEMail.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lecture.Migrations
+{
+    public partial class AddStringLengthToEMail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Professors",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Professors",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+        }
+    }
+}
